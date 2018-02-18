32bda4d443950c19715f272435d5497cc0cca11a
Anton Zuev <anzuev@bk.ru>
2018-02-18 16:41:04 +0300

    dms init

--- run.py
+++ run.py
@@ -1,47 +1,7 @@
-from entities import *
-from dms import *
-from devices import *
-from etc import *
 from running_support import *
 
-dms = DMS(50)
-village = Village(dms)
-dms.village = village
-dms.start()
-sockets = get_all_sockets(dms)
+dms = init_dms()
 
-sockets_for_flat_1 = sockets[0]
-sockets_for_flat_2 = sockets[1]
-
-
-lamp_socket = sockets_for_flat_1[0]
-lamp_socket.attach_device(NonSwitchableDevice("Lamp", 1, -1))
-
-wm_socket = sockets_for_flat_1[1]
-wm_socket.attach_device(ShiftableDevice("Washing Machine", 4, 60))
-
-kettle_socket_1 = sockets_for_flat_1[2]
-kettle_socket_1.attach_device(NonSwitchableDevice("Kettle1", 2, 10))
-
-kettle_socket_2 = sockets_for_flat_1[3]
-kettle_socket_2.attach_device(NonSwitchableDevice("Kettle2", 3, 10))
-
-kettle_socket_3 = sockets_for_flat_1[4]
-kettle_socket_3.attach_device(NonSwitchableDevice("Kettle3", 2.2, 10))
-
-boiler_socket_1 = sockets_for_flat_2[0]
-boiler_socket_1.attach_device(InterruptibleDevice("Boiler1", 2))
-
-boiler_socket_2 = sockets_for_flat_2[1]
-boiler_socket_2.attach_device(InterruptibleDevice("Boiler2", 2))
-
-lamp_socket.start_by_user()
-kettle_socket_1.start_by_user()
-boiler_socket_1.start_by_user()
-boiler_socket_2.start_by_user()
-wm_socket.start_by_user()
-kettle_socket_2.start_by_user()
-kettle_socket_3.start_by_user()
-lamp_socket.switch_off()
+f = 32
 
 
--- running_support.py
+++ running_support.py
@@ -1,4 +1,6 @@
 from dms import DMS
+from entities import *
+from devices import *
 
 
 def get_all_sockets(dms:DMS):
@@ -6,18 +8,86 @@ def get_all_sockets(dms:DMS):
 
     for house in dms.village.houses:
         for flat in house.flats:
-            result.append(flat.sockets)
+            result.extend(flat.sockets)
     return result
 
 
-def create_n_thermal_devices(n, base_title, power, dms):
-    pass
+def create_n_thermal_devices(n, base_title, power, sockets):
+    added_devices = 0
+    id = 1000
+    for socket in sockets:
+        if socket.device:
+            continue
+        device = ThermalDevice("{}_{}".format(base_title, id), power)
+        id += 1
+        socket.attach_device(device)
+        socket.start_by_user()
+        added_devices += 1
+        if added_devices >= n:
+            break
+    print("Added {} thermal devices".format(added_devices))
 
-def create_n_non_switchable_devices(n, base_title, power, dms):
-    pass
 
-def create_n_interruptable_devices(n, base_title, power, dms):
-    pass
+def create_n_non_switchable_devices(n, base_title, power, sockets):
+    added_devices = 0
+    id = 100
+    for socket in sockets:
+        if socket.device:
+            continue
+        device = NonSwitchableDevice("{}_{}".format(base_title, id), power, -1)
+        id += 1
+        socket.attach_device(device)
+        socket.start_by_user()
+        added_devices += 1
+        if added_devices >= n:
+            break
+    print("Added {} non switchable devices".format(added_devices))
 
-def create_n_shiftable_devices(n, base_title, power, dms):
-    pass
+
+def create_n_interruptable_devices(n, base_title, power, sockets):
+    added_devices = 0
+    id = 500
+    for socket in sockets:
+        if socket.device:
+            continue
+        device = InterruptibleDevice("{}_{}".format(base_title, id), power)
+        id += 1
+        socket.attach_device(device)
+        socket.start_by_user()
+        added_devices += 1
+        if added_devices >= n:
+            break
+    print("Added {} non interruptible devices".format(added_devices))
+
+
+def create_n_shiftable_devices(n, base_title, power, sockets):
+    added_devices = 0
+    id = 5000
+    for socket in sockets:
+        if socket.device:
+            continue
+        device = InterruptibleDevice("{}_{}".format(base_title, id), power)
+        id += 1
+        socket.attach_device(device)
+        socket.start_by_user()
+        added_devices += 1
+        if added_devices >= n:
+            break
+    print("Added {} non shiftable devices".format(added_devices))
+
+
+def init_dms():
+    dms = DMS(200)
+    village = Village(dms)
+    dms.village = village
+    dms.start()
+    sockets = get_all_sockets(dms)
+
+    create_n_thermal_devices(24, "AC", 2, sockets)
+    create_n_interruptable_devices(24, "Boiler", 2, sockets)
+    create_n_non_switchable_devices(100, "Lamp", 0.5, sockets)
+    create_n_shiftable_devices(10, "Washing machine", 2, sockets)
+
+    return dms
+
+
